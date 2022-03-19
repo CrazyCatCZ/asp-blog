106b16ba246b48d44d05fc4ab3e42768cdd59df2
Jakub Plecháč <loxtox74@gmail.com>
2022-03-19 09:30:49 +0100

    Vytvořen Clanek.cs a Uzivatel.cs

---
+++ BlogApp/BlogApp/Models/Clanek.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Models
+{
+    public class Clanek
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = String.Empty;
+        [Required]
+        public string Content { get; set; } = String.Empty;
+        [Required]
+        public DateTime? Termin { get; set; }
+    }
+}
---
+++ BlogApp/BlogApp/Models/Uzivatel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Models
+{
+    public class Uzivatel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; } = String.Empty;
+        [Required]
+        public string Password { get; set; } = String.Empty;
+    }
+}
